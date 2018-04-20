7115f4d2b77db7ea13f2fae262ee9df375a6e81b
Untitled <john.d.reddock@gmail.com>
2018-04-20 15:45:31 -0400

    draw board

--- slide-puzzle/slide.py
+++ slide-puzzle/slide.py
@@ -37,3 +37,32 @@ DOWN = 'down'
 LEFT = 'left'
 RIGHT = 'right'
 
+def main():
+    global FPSCLOCK, DISPLAYSURF, RESET_SURF, RESET_RECT, NEW_SURF, NEW_RECT, SOLVE_SURF, SOLVE_RECT
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+
+def drawBoard(board, message):
+    DISPLAYSURF.fill(BGCOLOR)
+    if message: 
+        textSurf, textRect = makeText(message, MESSAGECOLOR, BGCOLOR, 5, 5)
+
+        DISPLAYSURF.blit(textSurf, textRect)
+    
+    for tilex in range(len(board)):
+        for tiley in range(len(board[0])):
+            if board[tilex][tiley]:
+                drawTile(tilex, tiley, board[tilex][tiley])
+    
+    left, top = getLeftTopOfTile(0,0)
+    width = BOARDWIDTH * TILESIZE
+    height = BOARDHEIGHT * TILESIZE
+    pygame.draw.rect(DISPLAYSURF, BORDERCOLOR, (left - 5, top - 5, width + 11, height + 11), 4)
+    
+    DISPLAYSURF.blit(RESET_SURF, RESET_RECT)
+    DISPLAYSURF.blit(NEW_SURF, NEW_RECT)
+    DISPLAYSURF.blit(SOLVE_SURF, SOLVE_RECT)
+
+
+

cce62fdd8c0d181726cfda1d0d5c0ae94900f6bc
Untitled <john.d.reddock@gmail.com>
2018-04-21 14:59:27 -0400

    starting board

--- slide-puzzle/slide.py
+++ slide-puzzle/slide.py
@@ -64,5 +64,20 @@ def drawBoard(board, message):
     DISPLAYSURF.blit(NEW_SURF, NEW_RECT)
     DISPLAYSURF.blit(SOLVE_SURF, SOLVE_RECT)
 
+def getStartingBoard():
+    #return a board data structure with tiles in the solved state
+    #for example, if BOARDWIDTH and BOARDHEIGHT are both 3, this function
+    #returns[[1,4,7], [2,5,8], [3,6,None]]
+    counter = 1
+    board = []
+    for x in range(BOARDWIDTH): 
+        column = []
+        for y in range(BOARDHEIGHT):
+            column.append(counter)
+            counter +=  BOARDWIDTH
+        board.append(column)
+        counter -= BOARDWIDTH * (BOARDHEIGHT - 1) + BOARDWIDTh - 1
 
-
+    board[BOARDWIDTH-1][BOARDHEIGHT-1] = None
+    return board
+    

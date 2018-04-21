954d2fbde58025169c7cf62f2709966aacd71a3f
Untitled <john.d.reddock@gmail.com>
2018-04-21 15:14:04 -0400

    make a move

--- slide-puzzle/slide.py
+++ slide-puzzle/slide.py
@@ -88,4 +88,15 @@ def getBlankPosition(board):
             if board[x][y] == None:
                 return (x,y)
 
+def makeMoveBoard(board, move):
+    #this function does not check if the move is valid
+    blankx, blanky = getBlankPosition(board)
 
+    if move == UP:
+        board[blankx][blanky], board[blankx][blanky + 1] = board[blankx][blanky + 1], board[blankx][blanky]
+    elif move == DOWN:
+        board[blankx][blanky], board[blankx][blanky - 1] = board[blankx][blanky - 1], board[blankx][blanky]
+    elif move == LEFT:
+        board[blankx][blanky], board[blankx + 1][blanky] = board[blankx + 1][blanky], board[blankx][blanky]
+    elif move == RIGHT:
+    board[blankx][blanky], board[blankx - 1][blanky] = board[blankx - 1][blanky], board[blankx][blanky]

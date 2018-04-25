73edc8b1ca040c3d882f896ae8c6842512bfb690
Untitled <john.d.reddock@gmail.com>
2018-04-25 14:00:30 -0400

    configuration

---
+++ simulate/simulate.py
@@ -0,0 +1,28 @@
+import random, sys, time, pygame
+from pygame.locals import *
+
+FPS = 30
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+FLASHSPEED = 500 # in milliseconds
+FLASHDELAY = 200 # in milliseconds
+BUTTONSIZE = 200
+BUTTONGAPSIZE = 20
+TIMEOUT = 4 # seconds before game over if no button is pushed.
+
+#                R    G    B
+WHITE        = (255, 255, 255)
+BLACK        = (  0,   0,   0)
+BRIGHTRED    = (255,   0,   0)
+RED          = (155,   0,   0)
+BRIGHTGREEN  = (  0, 255,   0)
+GREEN        = (  0, 155,   0)
+BRIGHTBLUE   = (  0,   0, 255)
+BLUE         = (  0,   0, 155)
+BRIGHTYELLOW = (255, 255,   0)
+YELLOW       = (155, 155,   0)
+DARKGRAY     = ( 40,  40,  40)
+bgColor = BLACK
+
+XMARGIN = int((WINDOWWIDTH - (2 * BUTTONSIZE) - BUTTONGAPSIZE) / 2)
+YMARGIN = int((WINDOWHEIGHT - (2 * BUTTONSIZE) - BUTTONGAPSIZE) / 2)

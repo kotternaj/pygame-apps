d3474a1bc1615cf43c8582909270582a4aa151a0
Untitled <john.d.reddock@gmail.com>
2018-04-27 13:01:16 -0400

    draw worm

--- wormy/wormy.py
+++ wormy/wormy.py
@@ -52,3 +52,11 @@ def checkForKeyPress():
         terminate()
     return keyUpEvents[0].key
 
+def drawWorm(wormCoords):
+    x = coord['x'] * CELLSIZE
+    y = coord['y'] * CELLSIZE
+    wormSegmentRect = pygame.Rect(x, y, CELLSIZE, CELLSIZE)
+    pygame.draw.rect(DISPLAYSURF, DARKGREEN, wormSegmentRect)
+    wormInnerSegmentRect = pygame.Rect(x + 4, y + 4, CELLSIZE - 8, CELLSIZE - 8)
+    pygame.draw.rect(DISPLAYSURF, GREEN, wormInnerSegmentRect)
+

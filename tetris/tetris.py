4dcd432210ffd02472786b3dc7b7ddebafc81b52
Untitled <john.d.reddock@gmail.com>
2018-04-30 21:17:06 -0400

    check for key press

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -157,3 +157,14 @@ def terminate():
     pygame.quit()
     sys.exit()
 
+def checkForKeyPress():
+    # Go through event queue looking for a KEYUP event.
+    # Grab KEYDOWN events to remove them from the event queue.
+    checkForQuit()
+
+    for event in pygame.event.get([KEYDOWN, KEYUP]):
+        if event.type == KEYDOWN:
+            continue
+        return event.key 
+    return None
+

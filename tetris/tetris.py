d9b760f2ddd3f119b69108972de76a7bcb34368e
Untitled <john.d.reddock@gmail.com>
2018-04-30 21:23:48 -0400

    calc level & fall freq

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -175,4 +175,11 @@ def getBlankBoard():
         board.append([BLANK] * BOARDHEIGHT)
     return board
 
+def calculateLevelAndFallFreq(score):
+    # based on the score, return the level the player is on and 
+    # how many secs pass until a falling piece falls one space
+    level = int(score / 10) + 1
+    fallFreq = 0.27 - (level * 0.02)
+    return level, fallFreq
+
 
